004a34fc5e24f677c3026826d220ee52234fa71e
Jerry Zhu <zmx201518@gmail.com>
2024-01-25 09:59:49 -0800

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol:'https',
-                hostname: 'media.graphassets.com',
-            },
-        ],
-    },
+    output: "export",
 }
 
 module.exports = nextConfig
